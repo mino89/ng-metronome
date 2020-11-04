e39cff2f8802115006fe5bd08fe4c437a5383214
mino89 <pierdomenicoreitano@gmail.com>
2020-11-05 00:36:24 +0100

    created metronome web worker and implemented sounds with web audio api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { count } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ng-mertronome';
   timer = null;
-  audio = new Audio('../assets/metro_mine/MetroBar1.wav');
-  audioStress = new Audio('../assets/metro_mine/MetroBeat1.wav');
+  audio = new Audio('../assets/metro_frank/metro_1.wav');
+  audioStress = new Audio('../assets/metro_frank/metro_other.wav');
   stress = false;
   bpm = 60;
-  beats = 1;
+  beats = 2;
+  frequency = 800;
   switchClass = false;
-  ngOnInit() {
-
-  }
-
-  start(bpm, beats) {
-    this.stop();
-    this.timer = this.metronome_engine(bpm, beats);
-  }
-
-  stop() {
-    return clearInterval(this.timer);
-  }
-
-  metronome_engine(bpm, beats) {
-    let counter = 0;
-    return setInterval(() => {
-      if (counter == beats) {
-        counter = 1;
-      } else {
-        counter++;
-      }
-      this.beat_maker(counter);
-      this.switchClass = !this.switchClass;
-    }, this.calculate_bpm(bpm));
-  }
-
-  beat_maker(counter) {
-    if (this.stress) {
-      if (counter <= 1) {
-        console.log('primo');
-        this.audioStress.play()
-      } else {
-        this.audio.play()
-        console.log('bit');
-      }
+  worker: Worker;
+  private audioCtx = new (window['AudioContext'] || window['webkitAudioContext'])();
+  constructor() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      this.worker = new Worker('./app.worker', { type: 'module' });
+      this.worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data.command}`);
+        this.sound(data.count, data.beats)
+      };
     } else {
-      this.audio.play()
-      console.log('bit');
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
     }
   }
 
-  calculate_bpm(val) {
-    return 60000 / val;
+  start(bpm, beats) {
+    this.worker.postMessage({
+      command: 'start',
+      bpm: bpm,
+      beats: beats,
+    });
+    // this.stop();
+    // this.timer = this.metronome_engine(bpm, beats);
   }
 
+  stop() {
+    this.worker.postMessage({
+      command: 'stop'
+    });
+    // return clearInterval(this.timer);
+  }
+
+  sound(beatNumber, beats) {
+    console.log(beatNumber, beatNumber % beats);
+    const osc = this.audioCtx.createOscillator();
+    const envelope = this.audioCtx.createGain();
+
+    osc.frequency.value = (beatNumber % beats === 1 && this.stress) ? this.frequency + 200 : this.frequency;
+    envelope.gain.value = 1;
+    envelope.gain.exponentialRampToValueAtTime(1, this.audioCtx.currentTime + 0.001);
+    envelope.gain.exponentialRampToValueAtTime(0.001, this.audioCtx.currentTime + 0.02);
+
+    osc.connect(envelope);
+    envelope.connect(this.audioCtx.destination);
+
+    osc.start(this.audioCtx.currentTime);
+    osc.stop(this.audioCtx.currentTime + 0.03);
+  }
 }
+
+
---
+++ src/app/app.worker.ts
@@ -0,0 +1,23 @@
+/// <reference lib="webworker" />
+let timer;
+let counter = 0;
+function calculate_bpm(val) {
+  return 60000 / val;
+}
+
+addEventListener('message', ({ data }) => {
+  const response = data;
+  if (data.command === 'start') {
+    clearInterval(timer);
+    timer = setInterval(() => {
+      counter++;
+      data.count = counter;
+      postMessage(response);
+    }, calculate_bpm(data.bpm));
+  } else {
+    counter = 0;
+    clearInterval(timer);
+  }
+
+
+});
